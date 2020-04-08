6a52d612ca8180c5f3d47b2492107ea887d5d310
bcheng-weir <brian.cheng@mail.weir>
2020-04-08 16:57:04 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const server = express();
+const routes = require("./routes");
+const authMiddleware = require("./auth");
+
+server.use(cors());
+server.use(express.urlencoded({ extended: true, strict: false }));
+server.use(express.json());
+server.use(authMiddleware);
+
+server.get("/v1/printers", routes.doSomething);
+server.get("/", routes.getStatus);
+
+const port = process.env.port ? process.env.port : 80;
+
+server.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var axios = require("axios");
+
+module.exports = {
+  getStatus: (req, res) => {
+    res.json({ status: true });
+  },
+  doSomething: (req, res) => {
+    const getPrinters = async () => {
+      try {
+        const result = await axios.get(
+          process.env.URL,
+          { headers: { Authorization: `Bearer ${process.env.TOKEN}` } },
+          []
+        );
+        res.send(result.data);
+      } catch (e) {
+        res.send(500);
+      }
+    };
+    getPrinters();
+  },
+};
