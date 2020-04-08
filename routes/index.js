38c853ef0527f962bb4f68ed9720da8d36614075
bcheng-weir <brian.cheng@mail.weir>
2020-04-08 17:49:12 -0500

    replacing real call with some mocked data

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,21 @@ module.exports = {
     res.json({ status: true });
   },
   doSomething: (req, res) => {
-    const getPrinters = async () => {
-      try {
-        const result = await axios.get(
-          process.env.URL,
-          { headers: { Authorization: `Bearer ${process.env.TOKEN}` } },
-          []
-        );
-        res.send(result.data);
-      } catch (e) {
-        res.send(500);
-      }
-    };
-    getPrinters();
+    // const getPrinters = async () => {
+    //   try {
+    //     const result = await axios.get(
+    //       process.env.URL,
+    //       { headers: { Authorization: `Bearer ${process.env.TOKEN}` } },
+    //       []
+    //     );
+    //     res.send(result.data);
+    //   } catch (e) {
+    //     res.send(500);
+    //   }
+    // };
+    // getPrinters();
+    console.log(process.env.SAMPLEDATA);
+    let sample = JSON.parse(process.env.SAMPLEDATA);
+    res.send(sample);
   },
 };
